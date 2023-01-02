07863617b5b0d21d3818cbd0294b0ca9b3f4b403
ChrisCarucci <Chris.w.carucci@gmail.com>
2023-01-02 03:42:18 -0330

    GreenHouse Context Practice

---
+++ src/components/Greenhouse/LightSwitch.js
@@ -0,0 +1,17 @@
+import './LightSwitch.css';
+
+import { useTheme } from '../../context/ThemeContext.js'
+
+function LightSwitch() {
+
+  const { themeName, setThemeName } = useTheme();
+
+  return (
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={() => setThemeName('day')}>DAY</div>
+      <div className="off" onClick={() => setThemeName('night')}>NIGHT</div>
+    </div>
+  );
+}
+
+export default LightSwitch;
---
+++ src/context/ClimateContext.js
@@ -0,0 +1,29 @@
+// Temperature has a default value of 50 degrees
+// Humidity has a default value of 40%
+
+
+import { useContext, createContext, useState } from "react";
+
+
+export const ClimateContext = createContext();
+
+export const useClimate = () => useContext(ClimateContext)
+
+const ClimateProvider = props => {
+    const [ temperature, setTemperature ] = useState(50);
+    const [ humidity, setHumidity ] = useState(40)
+
+    return (
+        <ClimateContext.Provider 
+        value={{
+            temperature,
+            setTemperature,
+            humidity,
+            setHumidity
+        }}>
+            { props.children }
+        </ClimateContext.Provider>
+    )
+};
+
+export default ClimateProvider;
